9f42ea382b9b3b069f7bf98845d214739505aeeb
TomGhostSmith <TomGhostSmith@gmail.com>
2025-07-28 15:02:05 +0800

    DNABert MultiLength version

--- code/module/dnaLMRunner.py
+++ code/module/dnaLMRunner.py
@@ -36,6 +36,22 @@ class DNALMRunner():
         # self.manualLoadConfig = manualLoadConfig
         self.threads = threads
         self.max_seg_length = max_seg_length
+        self.batchSizes = []
+        self.max_seg_lengths = []
+        i = 256
+        # bsMultiply = [4, 3, 3, 3, 3, ...]
+        bs = batchSize
+        # initial = True
+        while i <= max_seg_length:
+            self.max_seg_lengths.append(i)
+            self.batchSizes.append(bs)
+            # if (initial):
+            #     bs = bs * 4
+            #     initial = False
+            # else:
+            bs = bs * 3
+            i = i * 2
+        self.batchSizes = list(reversed(self.batchSizes))
 
     def loadModel(self):
         manualLoadConfig = None
@@ -88,19 +104,30 @@ class DNALMRunner():
             trust_remote_code=True
         )
 
-        input_ids = []
-        real_masks = []
-        new_labels = []
+        # input_ids = []
+        # real_masks = []
+        # new_labels = []
+        data = {l: {
+            "labels": [],
+            "input_ids": [],
+            "real_mask": [],
+            "attention_mask": []
+        } for l in self.max_seg_lengths}
 
         threads = self.threads if (len(samples) > self.threads) else len(samples)
 
         if (threads == 1):
-            input_ids, real_masks, new_labels = tokenize_function(self.tokenizer, self.max_seg_length, sequences, labels)
+            input_ids, real_masks, new_labels = tokenize_function(self.tokenizer, self.max_seg_lengths, sequences, labels)
+            for l, inputList, realMaskList, newLabelList in zip(self.max_seg_lengths, input_ids, real_masks, new_labels):
+                data[l]["labels"] = newLabelList
+                data[l]["input_ids"] = inputList
+                data[l]["real_mask"] = realMaskList
+                data[l]["attention_mask"] = realMaskList > 0
         else:
             bs = math.ceil(len(samples)/threads)
             threads = math.ceil(len(samples) / bs)  # this can help adjust the number of threads if too few samples
             pbar = tqdm(total=threads, desc="tokenize")
-            jobs = [(self.tokenizer, self.max_seg_length, sequences[i * bs : (i+1) * bs], labels[i * bs : (i+1) * bs]) for i in range(threads)]
+            jobs = [(self.tokenizer, self.max_seg_lengths, sequences[i * bs : (i+1) * bs], labels[i * bs : (i+1) * bs]) for i in range(threads)]
             with multiprocessing.Pool(processes=threads) as pool:
                 asyncResults = [pool.apply_async(tokenize_function, param) for param in jobs]
                 # for i in range(threads):
@@ -112,49 +139,56 @@ class DNALMRunner():
                     for asyncResult in asyncResults[:]:
                         if (asyncResult.ready()):
                             segments, masks, newLabels = asyncResult.get()
-                            input_ids.append(segments)
-                            real_masks.append(masks)
-                            new_labels += newLabels
+                            for l, inputList, realMaskList, newLabelList in zip(self.max_seg_lengths, segments, masks, newLabels):
+                                if (len(newLabelList) != 0):
+                                    data[l]["labels"] += newLabelList
+                                    data[l]["input_ids"].append(inputList)
+                                    data[l]["real_mask"].append(realMaskList)
+                                    data[l]["attention_mask"].append(realMaskList > 0)
+                            # input_ids.append(segments)
+                            # real_masks.append(masks)
+                            # new_labels += newLabels
                             pbar.update(1)
 
                             asyncResults.remove(asyncResult)
                     time.sleep(1)
                 pool.join()
-            input_ids = torch.cat(input_ids, dim=0)
-            real_masks = torch.cat(real_masks, dim=0)
+            for l in self.max_seg_lengths:
+                if (len(data[l]["labels"]) != 0):
+                    data[l]["input_ids"] = torch.cat(data[l]["input_ids"], dim=0)
+                    data[l]["real_mask"] = torch.cat(data[l]["real_mask"], dim=0)
+                    data[l]["attention_mask"] = torch.cat(data[l]["attention_mask"], dim=0)
             pbar.close()
 
-        attn_masks = real_masks > 0
-
-
-        
-        testset = Dataset.from_dict({
-            "labels": new_labels,
-            "input_ids": input_ids,
-            "real_mask": real_masks,
-            "attention_mask": attn_masks
-        }).with_format("torch")
-
-        test_loader = DataLoader(testset, batch_size=self.batchSize)
+        dataLoaders = []
+        for l, bs in zip(self.max_seg_lengths, self.batchSizes):
+            if (len(data[l]["labels"]) != 0):
+                testset = Dataset.from_dict(data[l]).with_format("torch")
+                test_loader = DataLoader(testset, batch_size=bs)
+                dataLoaders.append(test_loader)
 
         self.loadModel()
 
         with torch.no_grad():
-            for batch in tqdm(test_loader, total=len(test_loader), desc="Inference"):
-                labels = batch['labels']
-                real_mask = batch["real_mask"].to(self.device)
-                batch = {k: v.to(self.device) for k, v in batch.items() if k != "labels" and k != "real_mask"}
+            for l, test_loader in zip(self.max_seg_lengths, dataLoaders):
+                for batch in tqdm(test_loader, total=len(test_loader), desc=f"{l} token inference"):
+                    labels = batch['labels']
+                    real_mask = batch["real_mask"].to(self.device)
+                    batch = {k: v.to(self.device) for k, v in batch.items() if k != "labels" and k != "real_mask"}
 
-                outputs = self.model(**batch, output_hidden_states=True)
-                last_hidden_state = outputs[0]
-                masks = real_mask.unsqueeze(-1)
-                masked_hidden = last_hidden_state * masks
-                sum_hidden = masked_hidden.sum(dim=1).detach().cpu().contiguous().numpy()
-                lengths = masks.sum(dim=1).detach().cpu().contiguous().numpy()
+                    outputs = self.model(**batch, output_hidden_states=True)
+                    last_hidden_state = outputs[0]
+                    masks = real_mask.unsqueeze(-1)
+                    masked_hidden = last_hidden_state * masks
+                    sum_hidden = masked_hidden.sum(dim=1).detach().cpu().contiguous().numpy()
+                    lengths = masks.sum(dim=1).detach().cpu().contiguous().numpy()
 
-                for idx, label in enumerate(labels):
-                    embeddings[label] += sum_hidden[idx]
-                    totalCounts[label] += lengths[idx]
+                    for idx, label in enumerate(labels):
+                        embeddings[label] += sum_hidden[idx]
+                        totalCounts[label] += lengths[idx]
+                if (torch.cuda.is_available()):
+                    torch.cuda.empty_cache()
+                    
 
         for k, v in embeddings.items():
             embeddings[k] = v / totalCounts[k]
@@ -162,31 +196,23 @@ class DNALMRunner():
     
     def clean(self):
         del self.model
-        torch.cuda.empty_cache()
-        torch.cuda.synchronize()
+        if (torch.cuda.is_available()):
+            torch.cuda.empty_cache()
+            torch.cuda.synchronize()
         gc.collect()
     
-def tokenize_function(tokenizer, max_seg_length, sequences, labels):
+def tokenize_function(tokenizer, max_seg_lengths, sequences, labels):
     idLists = tokenizer(sequences)["input_ids"]
-    newLabels = []
-    segments = []
-    masks = []
+    newLabels = [[] for _ in max_seg_lengths]
+    segments = [[] for _ in max_seg_lengths]
+    masks = [[] for _ in max_seg_lengths]
+    maxLength = max_seg_lengths[-1]
     for tokens, label in zip(idLists, labels):
         tokens = torch.tensor(tokens)
-        stride = max_seg_length // 2
-        segLength = max_seg_length
 
-        if (len(tokens) <= segLength):
-            segment = tokens
-            segLen = segment.shape[0]
-            padLen = segLength - segLen
-            segment = torch.cat([segment, torch.full((padLen, ), tokenizer.pad_token_id, dtype=segment.dtype)])
-            mask = torch.ones(segLength, dtype=torch.float16)
-            mask[segLen:] = 0
-            segments.append(segment)
-            masks.append(mask)
-            newLabels.append(label)
-        else:
+        if (len(tokens) > maxLength):
+            stride = maxLength // 2
+            segLength = maxLength
             for start in range(0, len(tokens) - stride, stride):
                 end = start + segLength
                 segment = tokens[start:end]
@@ -202,9 +228,25 @@ def tokenize_function(tokenizer, max_seg_length, sequences, labels):
                 elif end > len(tokens):
                     mask[stride : segLen] = 1
                     mask[segLen:] = 0
-                segments.append(segment)
-                masks.append(mask)
-                newLabels.append(label)
-    segments = torch.stack(segments)
-    masks = torch.stack(masks)
+                segments[-1].append(segment)
+                masks[-1].append(mask)
+                newLabels[-1].append(label)
+        else:
+            for bin_id, segLength in enumerate(max_seg_lengths):
+                if (len(tokens) <= segLength):
+                    segment = tokens
+                    segLen = segment.shape[0]
+                    padLen = segLength - segLen
+                    segment = torch.cat([segment, torch.full((padLen, ), tokenizer.pad_token_id, dtype=segment.dtype)])
+                    mask = torch.ones(segLength, dtype=torch.float16)
+                    mask[segLen:] = 0
+                    segments[bin_id].append(segment)
+                    masks[bin_id].append(mask)
+                    newLabels[bin_id].append(label)
+                    break
+    
+    for bin_id in range(len(max_seg_lengths)):
+        if (len(newLabels[bin_id]) != 0):
+            segments[bin_id] = torch.stack(segments[bin_id])
+            masks[bin_id] = torch.stack(masks[bin_id])            
     return segments, masks, newLabels
